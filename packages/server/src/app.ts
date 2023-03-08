5b684e01054ea63109fa268db550831275373a05
ShyRobin <shy_robin@163.com>
2023-03-08 11:18:14 +0800

    feat: add eslint and prettier

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,10 +1,10 @@
-import Koa from "koa";
-import { PORT } from "./config/constant";
+import Koa from 'koa'
+import { PORT } from './config/constant'
 
-const app = new Koa();
+const app = new Koa()
 
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT} is running...`);
-});
+  console.log(`http://localhost:${PORT} is running...`)
+})
 
-export default app;
+export default app
